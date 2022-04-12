793ec0c611de897e54e5ebba2a65ae49bdbba586
vanta blanta <vantablanta@gmail.com>
2022-04-12 13:42:43 +0300

    template form and footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { FormComponent } from './component/form/form.component';
+import { TrackingComponent } from './component/tracking/tracking.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormComponent,
+    TrackingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
