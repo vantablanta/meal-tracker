79c52adfb4e999cf895c99f24d799f2ca7ef668a
vanta blanta <vantablanta@gmail.com>
2022-04-12 13:57:57 +0300

    meal defining class

--- src/app/component/form/form.component.ts
+++ src/app/component/form/form.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Meal } from 'src/app/meal';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
+  meals!: Meal []
 
   constructor() { }
 
---
+++ src/app/meal.ts
@@ -0,0 +1,14 @@
+export class Meal {
+    name!: string
+    calories!: string
+    decsription!: string
+
+    constructor(name:string, calories:string, description: string){
+        this.name = name;
+        this.calories = calories
+        this.decsription = description
+    }
+
+    
+
+}
